9dac48f7de0f3d16bd2a46eec8df054a839368eb
Natalia Prins <nmacevedop@unal.edu.co>
2023-12-21 15:37:39 -0500

    download data yahoo

---
+++ src/data/copy_data_from_raw_to_intermediate.py
@@ -0,0 +1,52 @@
+# pylint: disable=invalid-name
+"""Copy all files from raw/ to intermediate/ removing NA.
+
+>>> from .copy_data_from_raw_to_intermediate import copy_data_from_raw_to_intermetidate
+>>> copy_data_from_raw_to_intermetidate("yahoo")
+--MSG-- File saved to msft.csv
+--MSG-- File saved to googl.csv
+--MSG-- File saved to aapl.csv
+--MSG-- All files copied from raw/ to intermediate/ removing NA.
+
+"""
+
+import os, sys
+import pandas as pd
+
+#TODO cambiar la ruta de ROOT_DIR_PROJECT por ruta relativa
+#ROOT_DIR_PROJECT = "/Users/nataliaacevedo/modelo_tesis_volatilidad/data/yahoo"
+
+def copy_data_from_raw_to_intermetidate(root_dir):
+   
+    """Copy all files from raw/ to intermediate/ removing NA."""
+
+    # Get the list of files in raw/
+    raw_files = os.listdir(os.path.join(root_dir, "raw"))
+
+    # Process each file in raw/
+    for raw_file in raw_files:
+        # Read the file
+        df = pd.read_csv(
+            os.path.join(root_dir, "raw", raw_file),
+            parse_dates=True,
+            index_col=0,
+            encoding='utf-8'
+        )
+
+        # Remove NA
+        df.dropna(inplace=True)
+
+        # Save the file
+        df.to_csv(
+            os.path.join(root_dir, "intermediate", raw_file),
+            index=True,
+        )
+        print(f"--MSG-- File saved to {raw_file}")
+
+    # Print message
+    print("--MSG-- All files copied from raw/ to intermediate/ removing NA.")
+
+if __name__ == '__main__':
+    copy_data_from_raw_to_intermetidate(
+        root_dir =  os.path.abspath('data/yahoo')
+    )
---
+++ src/data/download_data_from_yahoo.py
@@ -0,0 +1,64 @@
+"""
+Este script descarga los precios de cierre ajustados de las acciones de yahoo finance.
+
+--MSG-- Stock prices saved to data/yahoo/raw/aapl.csv
+--MSG-- Stock prices saved to data/yahoo/raw/msft.csv
+--MSG-- Stock prices saved to data/yahoo/raw/googl.csv
+
+"""
+
+import os
+import sys
+
+import pandas as pd
+import yfinance as yf
+
+# TODO cambiar la ruta de ROOT_DIR_PROJECT por ruta relativa-
+# ROOT_DIR_PROJECT = "/Users/nataliaacevedo/modelo_tesis_volatilidad/data/yahoo"
+
+
+def download_data_from_yahoo(
+    stocks_list,
+    start_date,
+    end_date,
+    root_dir,
+):
+    """Download specified stocks from Yahoo Finance and save them to individual CSV files."""
+
+    # obtain the root path of the project
+    root_path = os.path.abspath(
+        os.path.join(os.getcwd(), os.pardir, "modelo_202312", "data")
+    )
+    print(root_path)
+
+    # Create the root directory if it does not exist
+    if not os.path.exists(os.path.join(root_path, root_dir, "raw")):
+        os.makedirs(os.path.join(root_path, root_dir, "raw"))
+        print(f"--MSG-- Created directory {root_path}/{root_dir}/raw")
+
+    # Process each stock in the stocks list. Stock is the nemotechnic of the stock
+    for stock_name in stocks_list:
+        #
+        data = yf.download(stock_name, start=start_date, end=end_date, progress=False)
+        data_frame = pd.DataFrame(
+            {"price": data["Adj Close"]},
+            index=pd.date_range(start=start_date, end=end_date),
+        )
+        # print(os.path.abspath(os.getcwd()))
+        file_path = os.path.abspath("data/yahoo/raw")
+        file_path = os.path.join(file_path, "data_" + stock_name.lower() + ".csv")
+        # print (file_path)
+        data_frame.to_csv(file_path, index=True)
+        print(f"--MSG-- Stock prices saved to {file_path}")
+
+
+# TODO: Revisar el código de este script posiblemente quitar el data del nombre del archivo.
+
+# Ejecutar como: python3 -m download_data_from_yahoo
+if __name__ == "__main__":
+    download_data_from_yahoo(
+        stocks_list=["AAPL", "MSFT", "GOOGL"],
+        start_date="2020-01-01",
+        end_date="2020-12-31",
+        root_dir="yahoo",
+    )
