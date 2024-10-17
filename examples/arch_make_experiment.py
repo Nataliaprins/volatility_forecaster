2107abdaff2de6fcdcb9e320cb95cb63062c2e37
Natalia Prins <nmacevedop@unal.edu.co>
2024-10-17 18:06:09 -0500

    Refactor code to remove unused

--- examples/arch_make_experiment.py
+++ examples/arch_make_experiment.py
@@ -25,19 +25,3 @@ make_experiments(
     train_size=0.8,
     fit_params_combinations=fit_params_combinations,
 )
-
-# forecast one step
-forecast_one_step_ahead = forecast_one_step(
-    stock_name="googl",
-    logged_model_path="file:///Users/nataliaacevedo/volatility_forecaster/data/yahoo/models/mlflow/mlruns/449779083138429514/5c7d7c596dd54552911df045b2a55300/artifacts/artifacts",
-    column_name="log_yield",
-)
-print(forecast_one_step_ahead)
-
-# forecast n steps
-forecast_n_steps_ahead = forecast_n_steps(
-    stock_name="googl",
-    logged_model_path="file:///Users/nataliaacevedo/volatility_forecaster/data/yahoo/models/mlflow/mlruns/449779083138429514/10792a8e5d0341e493f4c5905f459963/artifacts/best_estimator",
-    column_name="log_yield",
-    n_steps=5,
-)
